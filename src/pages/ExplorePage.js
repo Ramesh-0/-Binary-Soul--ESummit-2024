620b9a34b37ac5e74abd975b4cbef366889a2975
Rames Kumar Singh <rameshkumarsingh121069@gmail.com>
2024-11-17 04:17:55 +0530

    Explore Page on landing page updated

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -49,26 +49,26 @@ function ExplorePage() {
             <div className="flex items-center px-4 py-2 bg-gray-100">
               <div className="w-1/4 text-gray-900">2</div>
               <div className="w-1/2 text-gray-800">Preamble and Fundamental Rights</div>
-              <a href="#link2" className="w-1/4 text-blue-500 hover:underline">Read More</a>
+              <a href="https://1drv.ms/w/c/6e6ca818a5e4a8a8/EeWnOs86r5tEpf6sEbetzIsBHWPTXigwuYBMxmOmK0PXzg?e=88kc4h" className="w-1/4 text-blue-500 hover:underline">Read More</a>
             </div>
             {/* Row 3 */}
             <div className="flex items-center px-4 py-2">
               <div className="w-1/4 text-gray-900">3</div>
               <div className="w-1/2 text-gray-800">Duties and Directive Principles</div>
-              <a href="#link3" className="w-1/4 text-blue-500 hover:underline">Read More</a>
+              <a href="https://1drv.ms/w/c/6e6ca818a5e4a8a8/EYq9iAWLpHpMjDowlmkmAAMBqVlP_uGu_riNNo_2J-k9WA?e=Wa8Y7D" className="w-1/4 text-blue-500 hover:underline">Read More</a>
             </div>
             {/* Additional Rows */}
             {[
               { id: 4, content: 'Amendments to the Constitution', link: '#link4' },
-              { id: 5, content: 'Separation of Powers', link: '#link5' },
-              { id: 6, content: 'Federalism and State Relations', link: '#link6' },
-              { id: 7, content: 'Judicial Review and Supreme Court', link: '#link7' },
-              { id: 8, content: 'Legislature and Parliament', link: '#link8' },
-              { id: 9, content: 'Executive Powers and President', link: '#link9' },
-              { id: 10, content: 'Constitutional Bodies', link: '#link10' },
+              { id: 5, content: 'Separation of Powers', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EeeBtAhvIhBGo33DPugzCfkBjH-f6u7ArQ6DVFi3z1TDbQ?e=FZb791' },
+              { id: 6, content: 'Federalism and State Relations', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EW9JrLpuHBhDpXlkuGiacXoB-ImngjVrhxcvWHwIpws7eg?e=a9vzga' },
+              { id: 7, content: 'Judicial Review and Supreme Court', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/ES7dWv5_IaNOpPGlpMzjx5cBSFTFCeoeCyIh7sub74SPOg?e=tLeDaL' },
+              { id: 8, content: 'Legislature and Parliament', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/Ee3OHgx6u6BIqkC01h8yUxYBHnmQL2KwT3aboxnUHHsG-g?e=KK7OeG' },
+              { id: 9, content: 'Executive Powers and President', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EfHtV6FvTbdCv3sp3RdED3IBRNDtD8CJmOc4aHuZa72MrQ?e=mg4x2Y' },
+              { id: 10, content: 'Constitutional Bodies', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EbBbpKqS09NDkdRGHbwkAyIBfqda-a33d0HU_RfBE9kk3A?e=PwblYA' },
               { id: 11, content: 'Schedules of the Constitution', link: '#link11' },
-              { id: 12, content: 'Election Laws and Procedures', link: '#link12' },
-              { id: 13, content: 'Emergency Provisions', link: '#link13' },
+              { id: 12, content: 'Election Laws and Procedures', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EYSLrbSxXSpLvOF1JLzwXiABeVj94iGxMIUw-NfWii3SqA?e=9ZJErs' },
+              { id: 13, content: 'Emergency Provisions', link: 'https://1drv.ms/w/c/6e6ca818a5e4a8a8/EUZU-cifQ-ZKsRkI6LP5CLQBlG_805fJy_rWAXZr8Qvgow?e=a6EsUo' },
             ].map((row, index) => (
               <div key={row.id} className={`flex items-center px-4 py-2 ${index % 2 === 0 ? 'bg-gray-100' : ''}`}>
                 <div className="w-1/4 text-gray-900">{row.id}</div>
